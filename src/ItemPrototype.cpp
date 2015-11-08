d3782bb0d05f0b9d7699b0a1eec2e4676fa283ea
mookTungs <mook13168@hotmail.com>
2015-11-08 14:42:38 -0800

    added a derived Potion class

--- src/ItemPrototype.cpp
+++ src/ItemPrototype.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 
+//Abstract Class
 class Item
 {
 public:
 	virtual Item* clone() = 0;
 	virtual void printItem() = 0;
 };
+
+
+class Potion : public Item
+{
+public:
+	Item* clone()
+	{
+		return new Potion;
+	}
+	void printItem()
+	{
+		cout << "Potion is created\n";
+	}
+}
